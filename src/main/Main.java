697ca70ead85681e0badf1c59869c5bf0f0f308e
Dmytro <dmytro.kovalov.64@gmail.com>
2014-05-09 22:51:32 +0300

    create Main class

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * The main class of application. 
+ * 
+ * @author DKovalov
+ *
+ */
+public class Main
+{
+
+    /**
+     * The main method of application.
+     * 
+     * @param args
+     *            arguments from console
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
